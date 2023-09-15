50c79782fad68a2a3a3d7ab2f010089c928b5552
AAdhikari <45696248+Asquaredd@users.noreply.github.com>
2023-09-15 06:51:25 -0700

    v1

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,10 +1,10 @@
 import Project1 from "../../assets/project1_piano.webp";
-import Project2 from "<div styleName="" />
-<div styleName="" />
-<assets />
-<project_2></project_2>bmi.webp";
 import Project3 from "../../assets/project3_nasamemory.webp";
-
+import Project2 from "../../assets/project_2bmi.webp";
+import Project4 from "../../assets/project4_Gallery.webp";
+import Project5 from "../../assets/project5.webp";
+import Project6 from "../../assets/project6.webp";
+import Project7 from "../../assets/project7.webp";
 
 export const projectsData = [
     {
@@ -12,18 +12,48 @@ export const projectsData = [
         image: Project1,
         title: "Project 1",
         category: "A",
+        link: "https://asquaredd.github.io/bmiCalculator/"
     },
     {
         id: 2,
         image: Project2,
         title: "Project 2",
-        category: "B",
+        category: "IOT",
+        link: "https://asquaredd.github.io/bmiCalculator/",
     },
     {
         id: 3,
         image: Project3,
         title: "Project 3",
         category: "C",
+        link: "https://asquaredd.github.io/NASAcardMemoryGame/"
+    },
+    {
+        id: 4,
+        image: Project4,
+        title: "Project 4",
+        category: "A",
+        link: "https://asquaredd.github.io/ArtificialArtGallery/",
+    },
+    {
+        id: 5,
+        image: Project5,
+        title: "Project 5",
+        category: "A",
+        link: "https://asquaredd.github.io/sudoku/"
+    },
+    {
+        id: 6,
+        image: Project6,
+        title: "Project 6",
+        category: "A",
+        link: "https://asquaredd.github.io/membershipCard/",
+    },
+    {
+        id: 7,
+        image: Project7,
+        title: "DIY wifi thermostat",
+        category: "IOT",
     },
 ];
 
@@ -32,12 +62,21 @@ export const projectsNav = [
         name: "All",
     },
     {
-        name: "A",
+        name: "Vanila Js",
     },
     {
-        name: "B",
+        name: "React Js",
     },
     {
-        name: "C",
+        name: "VueJs",
+    },
+    {
+        name: ".NET",
+    },
+    {
+        name: "Python",
+    },
+    {
+        name: "IOT",
     },
 ];
