22e1db7cf79df1a139d17cd58fd1d61d899bffe1
AAdhikari <45696248+Asquaredd@users.noreply.github.com>
2023-09-22 22:27:34 -0700

    portfolio

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -5,40 +5,44 @@ import Project4 from "../../assets/project4_Gallery.webp";
 import Project5 from "../../assets/project5.webp";
 import Project6 from "../../assets/project6.webp";
 import Project7 from "../../assets/project7.webp";
+import Project8 from "../../assets/stepsqencer.png";
+import Project9 from "../../assets/blackjackgm.png";
+
+
 
 export const projectsData = [
     {
         id: 1,
         image: Project1,
-        title: "Project 1",
-        category: "A",
+        title: "Piano Web App",
+        category: "Vanila Js",
         link: "https://asquaredd.github.io/bmiCalculator/"
     },
     {
         id: 2,
-        image: Project2,
-        title: "Project 2",
-        category: "IOT",
+        image: Project8,
+        title: "Step-Seqencer",
+        category: "Vanila Js",
         link: "https://asquaredd.github.io/bmiCalculator/",
     },
     {
         id: 3,
         image: Project3,
-        title: "Project 3",
-        category: "C",
+        title: "NASA Memory Card Game",
+        category: "Vanila Js",
         link: "https://asquaredd.github.io/NASAcardMemoryGame/"
     },
     {
         id: 4,
-        image: Project4,
-        title: "Project 4",
+        image: Project9,
+        title: "Blackjack",
         category: "A",
         link: "https://asquaredd.github.io/ArtificialArtGallery/",
     },
     {
         id: 5,
         image: Project5,
-        title: "Project 5",
+        title: "Sudoku",
         category: "A",
         link: "https://asquaredd.github.io/sudoku/"
     },
@@ -51,6 +55,18 @@ export const projectsData = [
     },
     {
         id: 7,
+        image: Project2,
+        title: "BMI Calculator",
+        category: "Vanilla Js",
+    },
+    {
+        id: 8,
+        image: Project4,
+        title: "DIY wifi Artificial Intelligence Art Gallery",
+        category: "IOT",
+    },
+    {
+        id: 9,
         image: Project7,
         title: "DIY wifi thermostat",
         category: "IOT",
--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiCheckBadge } from "react-icons/hi2";
+import SVGIcon from './SVGIcon';
 
 const Backend = () => {
     return(
@@ -8,21 +9,21 @@ const Backend = () => {
         <div className="skills__box">
             <div className="skills__group">
                 <div className="skills__data">
-                    <HiCheckBadge />
+                    <SVGIcon name="Java" />
                     <div>
                         <h3 className="skills__name">Java</h3>
                         <span className="skills__level">Basic</span>
                     </div>
                 </div>
                 <div className="skills__data">
-                    <HiCheckBadge />
+                    <SVGIcon name="Csharp" />
                     <div>
                         <h3 className="skills__name">C#/.NET</h3>
                         <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
                 <div className="skills__data">
-                    <HiCheckBadge />
+                    <SVGIcon name="sql" />
                     <div>
                         <h3 className="skills__name">MySQL</h3>
                         <span className="skills__level">Basic</span>
@@ -31,21 +32,21 @@ const Backend = () => {
             </div>
             <div className="skills__group">
                 <div className="skills__data">
-                    <HiCheckBadge />
+                    <SVGIcon name="npm" />  
                     <div>
                         <h3 className="skills__name">npm</h3>
                         <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
                 <div className="skills__data">
-                    <HiCheckBadge />
+                    <SVGIcon name="node" />
                     <div>
                         <h3 className="skills__name">Node.js</h3>
                         <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
                 <div className="skills__data">
-                    <HiCheckBadge />
+                    <SVGIcon name="git" />
                     <div>
                         <h3 className="skills__name">Git</h3>
                         <span className="skills__level">Intermediate</span>
